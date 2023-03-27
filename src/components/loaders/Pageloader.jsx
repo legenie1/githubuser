0214c16e650e0b7e50607bf740dacf0681838e57
legenie1 <sergeomoffo1@gmail.com>
2023-03-27 23:36:51 +0200

    75% project setup

--- src/components/loaders/Pageloader.jsx
+++ src/components/loaders/Pageloader.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { ClipLoader } from "react-spinners";
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 function Pageloader() {
-  return <div></div>;
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+  return (
+    <div>
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 }
 
 export default Pageloader;
--- src/pages/landing/Landingpage.jsx
+++ src/pages/landing/Landingpage.jsx
@@ -1,29 +1,8 @@
-import React, { useState } from "react";
-import { ClipLoader } from "react-spinners";
+import React from "react";
 import Landinglayout from "../layouts/Landinglayout";
 
-const override = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
-
 function Landingpage() {
-  let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState("#ffffff");
-  return (
-    <Landinglayout>
-      This is the Home Page
-      <ClipLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-    </Landinglayout>
-  );
+  return <Landinglayout>This is the Home Page</Landinglayout>;
 }
 
 export default Landingpage;
